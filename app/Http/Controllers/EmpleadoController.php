fd66ea23764a22250002315367afc82d739da7f1
Michael Tapia <michaeltr65@gmail.com>
2022-12-14 13:09:52 -0300

    Cambio7 v1.4(se ajusta formulario de creación)

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Empleado;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Storage;
+
 class EmpleadoController extends Controller
 {
     /**
@@ -86,6 +88,13 @@ class EmpleadoController extends Controller
     {
         //
         $datosEmpleado = request()->except(['_token','_method']);
+        
+        if($request->hasFile('Foto')){
+            $empleado=Empleado::findOrFail($id);
+            Storage::delete('public/'.$empleado->Foto);
+            $datosEmpleado['Foto']=$request->file('Foto')->store('uploads','public');
+        }
+
         Empleado::where('id','=',$id)->update($datosEmpleado);
 
         $empleado=Empleado::findOrFail($id);
